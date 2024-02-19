1d9b1d7d6fc8ccbf04c57d068562b61be6e680bf
maciejtrebacz <maciekdmd90@gmail.com>
2024-02-19 20:51:36 +0100

    errors, handling errors

--- banking.go
+++ banking.go
@@ -1,14 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const accountBalanceFile = "balance.txt"
+
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
+}
+
+func getBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile(accountBalanceFile)
+	if err != nil {
+		return 1000, errors.New("failed to find balance file")
+	}
+
+	balanceText := string(data)
+	balance, _ := strconv.ParseFloat(balanceText, 64)
+	if err != nil {
+		return 1000, errors.New("Cannot convert it ")
+	}
+
+	return balance, nil
+}
 
 func main() {
-	var accountBalance float64 = 1000
+	var accountBalance, err = getBalanceFromFile()
+
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		panic(err)
+	}
+
 	// creating loop
 	for i := 0; i < 10; i++ {
 
 	}
-
 	const white = 100
 	for { // infinite loop
 
@@ -31,11 +64,13 @@ func main() {
 			var depositAmount float64
 			fmt.Scan(&depositAmount)
 			accountBalance += depositAmount
+			writeBalanceToFile(accountBalance)
 		case 3:
 			fmt.Print("How much to withdraw: ")
 			var withdrawAmount float64
 			fmt.Scan(&withdrawAmount)
 			accountBalance -= withdrawAmount
+			writeBalanceToFile(accountBalance)
 		case 4:
 			fmt.Print("Exiting")
 			return // we need to use return as break doesnt work in switch
